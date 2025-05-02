211a6a9346ad63cb66638b68d7fa70a5a2c076b0
AlekseyMusienko <79996261060@ya.ru>
2025-05-02 14:04:58 +0300

    Fix Headers decorator in telegram.controller.ts

--- src/telegram/telegram.controller.ts
+++ src/telegram/telegram.controller.ts
@@ -1,9 +1,9 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Headers } from '@nestjs/common';
 
-@Controller('webhook')
+@Controller('bot7583335421:AAEOHXw9RSntZWC1ER867nijCKynAOiNrDU')
 export class TelegramController {
   @Post()
-  async handleWebhook(@Body() update: any, @Headers() headers: any) {
+  async handleWebhook(@Body() update: any, @Headers() headers: Record<string, string>) {
     console.log('Function invoked at:', new Date().toISOString());
     console.log('Headers:', JSON.stringify(headers, null, 2));
     console.log('Body:', JSON.stringify(update, null, 2));
